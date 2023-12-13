c6dcbe7a9027964330c10546ba448e0ef440a7c1
Moa Sandberg <moisan1999@gmail.com>
2023-12-13 14:28:19 +0100

    first commit on project dahsboard, created files and some variables, basic html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+//Variabler för att hämta tid/datum med Date()
+const date = new Date()
+//Variabel för att konvertera date till en string så den senare kan printas ut snyggt
+const dateTimeString = date.toLocaleString();
+console.log(dateTimeString);
