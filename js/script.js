4c45c7febaf6438f7ce477ec812319c93c6c9830
Moa Sandberg <moisan1999@gmail.com>
2023-12-14 14:03:06 +0100

    updated fonts and aligned clock and h1 to center

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 
-//Variabler för att hämta tid/datum med Date()
-const date = new Date()
-//Variabel för att konvertera date till en string så den senare kan printas ut snyggt
-const dateTimeString = date.toLocaleString();
-console.log(dateTimeString);
+let dateDiv = document.getElementById("date-div");
+//Funktion för att uppdatera tiden utan att uppdatera sidan
+function updateTime(){
+    //Variabler för att hämta tid/datum med Date()
+    const date = new Date();
+    const dateTimeString = date.toLocaleString();
+    dateDiv.innerHTML = dateTimeString;
+
+}
+//Använder interval för att se till så tiden uppdateras varje sekund
+setInterval(updateTime, 1000);
+
+
+
+
+
+
+//https://fontawesome.com/search?q=weather&o=r länk till icons för väder
+
+//API för 3e rutan https://sv443.net/jokeapi/v2/      https://www.youtube.com/watch?v=xHuaEKCldhE&list=PLNCevxogE3fiLT6bEObGeVfHVLnttptKv
+
+//Väder-api https://openweathermap.org/appid
+
+
+//random bild API https://unsplash.com/developers
+
+//dokumentation för att spara h1 localstorage https://stackoverflow.com/questions/56735599/content-editable-not-saving-with-localstorage
