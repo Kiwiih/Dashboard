21a1adddb178fd2408599e8f7079a3a56f94af05
Moa Sandberg <moisan1999@gmail.com>
2023-12-14 15:57:12 +0100

    commented code, created multiple functions för localstorage and functionality of saving and editing html content

--- js/script.js
+++ js/script.js
@@ -6,15 +6,43 @@ function updateTime(){
     const date = new Date();
     const dateTimeString = date.toLocaleString();
     dateDiv.innerHTML = dateTimeString;
-
 }
 //Använder interval för att se till så tiden uppdateras varje sekund
 setInterval(updateTime, 1000);
 
+//Sektion för att uppdatera H1 och spara i localstorage
+let titleInput = document.getElementById('title');
+let titleKey = 'h1HTML';
+function updateTitleInput(){
+    localStorage.setItem(titleKey, titleInput.innerHTML);
+}
+/*Funktion för att när sidan laddats ska texten antingen innehålla default 
+'Dashboard' eller det som man själv fyllt i*/
+function inputContent() {
+    let result = localStorage.getItem(titleKey);
+    titleInput.innerHTML = result || 'Dashboard';
+};
+//Eventlistener för att funktionen ska kallas på när inputen är ändrad
+titleInput.addEventListener('input', updateTitleInput)
 
+//funktioner för notes inputen, så den uppdaterar och sparar i localStorage
+let notesInput = document.getElementById("notes-input");
+let notesKey = 'notesHTML';
 
+function updateNotesInput(){
+    localStorage.setItem(notesKey, notesInput.value);
+}
+function notesContent(){
+    let notesResult = localStorage.getItem(notesKey);
+    notesInput.value = notesResult || 'Skriv anteckningar här...';
+};
+notesInput.addEventListener('input', updateNotesInput)
 
-
+/*Funktion för att kalla på anteckningarna och titel funktionerna direkt när sidan laddas*/
+window.onload = () => {
+    inputContent(); 
+    notesContent();
+    }
 
 //https://fontawesome.com/search?q=weather&o=r länk till icons för väder
 
